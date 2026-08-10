896b108be2f2cffbc522402fb00a784f30a27955
David Schröder <post@schroederdavid.de>
2026-08-10 18:14:58 +0200

    modify LSU + ram: spec-compliant TileLink byte/halfword accesses + misalign logic

--- src/rtl/nanosoc/nanosoc_ram.sv
+++ src/rtl/nanosoc/nanosoc_ram.sv
@@ -23,7 +23,7 @@ module nanosoc_ram
     logic [        31:0] rdata;
 
     logic       stall; // response not accepted
-    logic       err_misalign;
+    logic       misalign_q, misalign_d;
     logic [7:0] source_q;
     tl_a_op_e   op_q;
     logic       tx_q;
@@ -46,9 +46,36 @@ module nanosoc_ram
         if (~stall) rdata <= mem[addr];
     end
 
+    always_comb begin
+        if (tl_i.a_opcode inside {Get, PutFullData}) begin
+            unique case ({tl_i.a_size, tl_i.a_mask, tl_i.a_address[1:0]})
+                {2'h0, 4'b0001, 2'b00},
+                {2'h0, 4'b0010, 2'b01},
+                {2'h0, 4'b0100, 2'b10},
+                {2'h0, 4'b1000, 2'b11},
+                {2'h1, 4'b0011, 2'b00},
+                {2'h1, 4'b1100, 2'b10},
+                {2'h2, 4'b1111, 2'b00}: misalign_d = '0;
+                default: misalign_d = '1;
+            endcase
+        end else begin
+            // PutPartialData
+            unique casez ({tl_i.a_size, tl_i.a_mask, tl_i.a_address[1:0]})
+                {2'h0, 4'b000?, 2'b00},
+                {2'h0, 4'b00?0, 2'b01},
+                {2'h0, 4'b0?00, 2'b10},
+                {2'h0, 4'b?000, 2'b11},
+                {2'h1, 4'b00??, 2'b00},
+                {2'h1, 4'b??00, 2'b10},
+                {2'h2, 4'b????, 2'b00}: misalign_d = '0;
+                default: misalign_d = '1;
+            endcase
+        end
+    end
+
     always_ff @(posedge clk_i or negedge rst_ni) begin
         if (~rst_ni) begin
-            err_misalign <= '0;
+            misalign_q <= '0;
             source_q <= '0;
             op_q <= Get;
             tx_q <= '0;
@@ -56,7 +83,7 @@ module nanosoc_ram
             if (~stall) begin
                 tx_q <= tl_i.a_valid;
                 op_q <= tl_i.a_opcode;
-                err_misalign <= |tl_i.a_address[1:0];
+                misalign_q <= misalign_d;
                 source_q <= tl_i.a_source;
             end
         end
@@ -66,7 +93,7 @@ module nanosoc_ram
         d_valid: tx_q,
         d_opcode: op_q == Get ? AccessAckData : AccessAck,
         d_data: rdata,
-        d_denied: err_misalign,
+        d_denied: misalign_q,
         d_source: source_q,
         a_ready: ~stall
     };
--- src/rtl/turvo32/turvo32_lsu.sv
+++ src/rtl/turvo32/turvo32_lsu.sv
@@ -29,31 +29,72 @@ module turvo32_lsu
     input  tl_d2h_t     tl_i
 );
 
+    /* Common access logic */
+
+    logic [3:0] mask;
+    logic [1:0] size;
+    tl_a_op_e   op;
+
+    always_comb begin
+        unique case (op_i)
+            LB,
+            LBU,
+            SB: size = 2'h0;
+            LH,
+            LHU,
+            SH: size = 2'h1;
+            LW,
+            SW: size = 2'h2;
+            default: size = 2'h0;
+        endcase
+
+        unique case (op_i)
+            LB,
+            LBU,
+            LH,
+            LHU,
+            LW: op = Get;
+            SB,
+            SH,
+            SW: op = PutFullData;
+            default: op = Get;
+        endcase
+
+        unique case ({op_i, address_i[1:0]})
+            {SB , 2'b00}: mask = 4'b0001;
+            {SB , 2'b01}: mask = 4'b0010;
+            {SB , 2'b10}: mask = 4'b0100;
+            {SB , 2'b11}: mask = 4'b1000;
+            {SH , 2'b00}: mask = 4'b0011;
+            {SH , 2'b10}: mask = 4'b1100;
+            {SW , 2'b00}: mask = 4'b1111;
+            {LB , 2'b00}: mask = 4'b0001;
+            {LB , 2'b01}: mask = 4'b0010;
+            {LB , 2'b10}: mask = 4'b0100;
+            {LB , 2'b11}: mask = 4'b1000;
+            {LBU, 2'b00}: mask = 4'b0001;
+            {LBU, 2'b01}: mask = 4'b0010;
+            {LBU, 2'b10}: mask = 4'b0100;
+            {LBU, 2'b11}: mask = 4'b1000;
+            {LH , 2'b00}: mask = 4'b0011;
+            {LH , 2'b10}: mask = 4'b1100;
+            {LHU, 2'b00}: mask = 4'b0011;
+            {LHU, 2'b10}: mask = 4'b1100;
+            {LW , 2'b00}: mask = 4'b1111;
+            // Misaligned cases don't access memory
+            default: mask = 4'b0000;
+        endcase
+    end
+
     /* Store logic */
 
-    logic [29:0] waddr; // word address
     logic [31:0] wdata;
-    logic [ 3:0] wmask;
-
-    assign waddr = address_i[31:2];
 
     always_comb begin
         unique case (op_i)
             SB: wdata = {data_i[7:0], data_i[7:0], data_i[7:0], data_i[7:0]};
             SH: wdata = {data_i[15:0], data_i[15:0]};
             default: wdata = data_i;
-        endcase
-
-        unique case ({op_i, address_i[1:0]})
-            {SB, 2'b00}: wmask = 4'b0001;
-            {SB, 2'b01}: wmask = 4'b0010;
-            {SB, 2'b10}: wmask = 4'b0100;
-            {SB, 2'b11}: wmask = 4'b1000;
-            {SH, 2'b00}: wmask = 4'b0011;
-            {SH, 2'b10}: wmask = 4'b1100;
-            {SW, 2'b00}: wmask = 4'b1111;
-            // Misaligned cases don't write to memory
-            default: wmask = 4'b0000;
         endcase
     end
 
@@ -130,10 +171,10 @@ module turvo32_lsu
 
     assign tl_o = '{
         a_valid: is_valid_mem_op && state_q == Idle,
-        a_opcode: |wmask ? (wmask == 4'hF ? PutFullData : PutPartialData) : Get,
-        a_address: {waddr, 2'h0},
-        a_size: 2'h2,
-        a_mask: wmask == 4'h0 ? 4'hF : wmask, // 4'hF for reads -> TileLink spec
+        a_opcode: op,
+        a_address: address_i,
+        a_size: size,
+        a_mask: mask,
         a_data: wdata,
         a_source: '0, // Only ever one outstanding request
         d_ready: '1
